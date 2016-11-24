c9b15868745a2a69015b9152b58129d484adfe4b
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-11-24 04:02:54 -0500

    [ WIP ] array - fields / countries select

--- src/App/resources/views/js-grid/js.blade.php
+++ src/App/resources/views/js-grid/js.blade.php
@@ -1,17 +1,44 @@
+@php
+
+    $countries = [
+        ['Name' => "", 'Id' => 0],
+        ['Name' => "United States", 'Id' => 1],
+        ['Name' => "Canada", 'Id' => 2],
+        ['Name' => "United Kingdom", 'Id' => 3]
+        ];
+
+        $fields = [
+        ['name' => 'Name', 'type' => 'text', 'width' => 150, 'validate' => 'required'],
+        ['name' => 'Age', 'type' => 'number', 'width' => 50,],
+        ['name' => 'Address', 'type' => 'text', 'width' => 200,],
+        ['name' => 'Country', 'type' => 'select', 'valueField' => 'Id', 'textField' => 'Name', 'items' => [
+        ['Name' => "", 'Id' => 0],
+        ['Name' => "United States", 'Id' => 1],
+        ['Name' => "Canada", 'Id' => 2],
+        ['Name' => "United Kingdom", 'Id' => 3]
+        ]
+        ],
+        ['name' => 'Married', 'type' => 'checkbox', 'title' => 'Is Married', 'sorting' => false],
+        ['type' => 'control'],
+        ];
+
+        dump(json_encode($fields));
+
+@endphp
 <script>
     var clients = [
-        { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
-        { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
-        { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
-        { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },
-        { "Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false }
+        {"Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false},
+        {"Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true},
+        {"Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false},
+        {"Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true},
+        {"Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false}
     ];
 
     var countries = [
-        { Name: "", Id: 0 },
-        { Name: "United States", Id: 1 },
-        { Name: "Canada", Id: 2 },
-        { Name: "United Kingdom", Id: 3 }
+        {Name: "", Id: 0},
+        {Name: "United States", Id: 1},
+        {Name: "Canada", Id: 2},
+        {Name: "United Kingdom", Id: 3}
     ];
     $("{{ $selector or "#jsGrid" }}").jsGrid({
         width: "100%",
@@ -24,13 +51,6 @@
 
         data: clients,
 
-        fields: [
-            { name: "Name", type: "text", width: 150, validate: "required" },
-            { name: "Age", type: "number", width: 50 },
-            { name: "Address", type: "text", width: 200 },
-            { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
-            { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
-            { type: "control" }
-        ]
+        fields: {!! json_encode($fields) !!}
     });
 </script>
